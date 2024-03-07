fb8837f2b3d08e8868a83c5f009b4f8db9f6ecbd
stuckwithprogression <chukwunwemichael@gmail.com>
2024-03-07 18:43:14 +0000

    Fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-"""
-
+"""Script that queries the Reddit API and returns the number of subscribers
+for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API
+    """Returns the total number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-User-Agent"}
 
-    Args:
-        subreddit: subreddit name
-
-    Return:
-        returns the number of subscribers
-    """
-
-    headers = {"User-Agent": "Custom"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-
-    return 0
+    response = requests.get(url,
+                            headers=headers,
+                            allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
+    else:
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
+        return subscribers
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""queries the Reddit API
-"""
+"""Script that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit."""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for a given subreddit
+    """Prints the titles of the first 10 hot posts listed for a given
+    subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {"User-Agent": "My-User-Agent"}
+    params = {
+        "limit": 10
+    }
 
-    Args:
-        subreddit: the given subreddit
-    """
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
-    headers = {"User-Agent": 'xica389'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {"limit": 10}
-    request = requests.get(url, headers=headers, allow_redirects=False,
-                           params=params)
-
-    if request.status_code == 200:
-        titles = request.json().get("data").get("children")
-
-        for title in titles:
-            print(title.get("data").get("title"))
-
+    if response.status_code >= 300:
+        print("None")
     else:
-        print(None)
+        [print(child.get("data").get("title"))
+         for child in response.json().get("data").get("children")]
